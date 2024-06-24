8a0f746340e761fe8a5db589203b6182077606cd
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-24 11:42:21 +0530

    client side routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layouts/Layout";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Search from "./pages/Search";
+import Gif from "./pages/Gif";
+import Favourites from "./pages/Favourites";
 import "./App.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path=":category" element={<Category />} />
+      <Route path="search/:query" element={<Search />} />
+      <Route path=":type/:slug" element={<Gif />} />
+      <Route path="favourites" element={<Favourites />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <>
-      <div>Jai Shree Ram</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Gif.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gif() {
+  return <div>Gif</div>;
+}
+
+export default Gif;
