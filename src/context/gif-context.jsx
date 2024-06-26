7a4070647d0f2ad4bff76b950458dae1f5804bd9
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-26 16:26:47 +0530

    gif context api key setup done

--- src/context/gif-context.jsx
+++ src/context/gif-context.jsx
@@ -1,9 +1,15 @@
-import { createContext } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { createContext, useContext } from "react";
 
 const GifContext = createContext();
 
 const GifProvider = ({ children }) => {
-  return <GifContext.Provider>{children}</GifContext.Provider>;
+  const gif = new GiphyFetch(import.meta.env.VITE_ANIBUZZ_API_KEY);
+  return <GifContext.Provider value={{ gif }}>{children}</GifContext.Provider>;
+};
+
+export const GifState = () => {
+  return useContext(GifContext);
 };
 
 export default GifProvider;
